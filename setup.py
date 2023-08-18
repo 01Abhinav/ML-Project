7ddfa27d622bf2e4181550381d2baf46697b88a6
Abhinav <abhinav.zimmy@gmail.com>
2023-08-18 18:54:45 +0530

    changed setup

--- setup.py
+++ setup.py
@@ -6,6 +6,9 @@ def get_requirements(file_path:str) -> List[str]:
     with open(file_path) as file_obj:
         requirements = file_obj.readlines()
         requirements = [req.replace("\n","") for req in requirements ]
+        
+        if "-e ." in requirements:
+            requirements.remove("-e .")
         return requirements
 setup(
     name = 'ML-project',
