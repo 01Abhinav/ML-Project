afb76c99c67f55371acf3216cfe8fab0aa83fc1b
Abhinav <abhinav.zimmy@gmail.com>
2023-08-18 18:50:48 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages,setup
+from typing import List
+
+def get_requirements(file_path:str) -> List[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","") for req in requirements ]
+        return requirements
+setup(
+    name = 'ML-project',
+    version='0.1',
+    author='Abhinav',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
